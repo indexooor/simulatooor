723bbd263529ccb0dc357f0e6170b37f83c811d6
Manav Darji <manavdarji.india@gmail.com>
2023-03-26 16:04:55 +0530

    refactor simulation

--- main.go
+++ main.go
@@ -4,95 +4,113 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
+
 	"net/http"
 	"os"
 	"strings"
+
+	log "github.com/inconshreveable/log15"
 )
 
 func main() {
-	monitor_config := getMonitorConfigs("monitor.json")
+	log.Info("Starting simulation...")
 
-	variables_to_index := monitor_config["variables"].([]interface{})
-	contractAddress := monitor_config["contractAddress"].(string)
-	for _, element := range variables_to_index {
+	config := getMonitorConfig("monitor.json")
+
+	variables := config["variables"].([]interface{})
+	contractAddress := config["contractAddress"].(string)
+
+	for _, element := range variables {
 		element := element.(map[string]interface{})
-		variable_snap := getVariable(element, contractAddress).(map[string]interface{})
-		vs := variable_snap["data"].([]interface{})
+		variableSnap, err := getVariable(element, contractAddress)
+		if err != nil {
+			continue
+		}
+		snap := variableSnap.(map[string]interface{})
+		vs := snap["data"].([]interface{})
 		vs1 := vs[0].(map[string]interface{})
-		log.Println(vs1["variableName"], vs1["variableType"], vs1["variableValue"])
+
+		log.Info("Indexed variable", "name", vs1["variableName"], "type", vs1["variableType"], "value", vs1["variableValue"])
 	}
 }
 
-func getMonitorConfigs(filename string) map[string]interface{} {
+func getMonitorConfig(filename string) map[string]interface{} {
 	jsonFile, err := os.Open(filename)
 	if err != nil {
-		log.Println(err)
+		log.Error("Error in reading file", "err", err)
 	}
 	defer jsonFile.Close()
 
 	byteValue, _ := ioutil.ReadAll(jsonFile)
 
-	var monitor_config map[string]interface{}
-	json.Unmarshal([]byte(byteValue), &monitor_config)
+	var config map[string]interface{}
+	json.Unmarshal([]byte(byteValue), &config)
 
-	return monitor_config
+	return config
 }
 
-func getVariable(variable_data map[string]interface{}, contractAddress string) interface{} {
+func getVariable(variableData map[string]interface{}, contractAddress string) (interface{}, error) {
 	var key = ""
 	var deep_key = ""
 	var struct_var = ""
-	if variable_data["key"] != nil {
-		key = variable_data["key"].(string)
+	if variableData["key"] != nil {
+		key = variableData["key"].(string)
 	}
-	if variable_data["deep_key"] != nil {
-		deep_key = variable_data["deep_key"].(string)
+	if variableData["deep_key"] != nil {
+		deep_key = variableData["deep_key"].(string)
 	}
-	if variable_data["struct_var"] != nil {
-		struct_var = variable_data["struct_var"].(string)
+	if variableData["struct_var"] != nil {
+		struct_var = variableData["struct_var"].(string)
 	}
 
 	payload := strings.NewReader(`{
-	"contractAddress": "` + fmt.Sprintf("%+v", contractAddress) + `",
-	"targetVariable": "` + fmt.Sprintf("%+v", variable_data["name"]) + `",
-	"key": "` + fmt.Sprintf("%+v", key) + `",
-	"deepKey": "` + fmt.Sprintf("%+v", deep_key) + `",
-	"structVar": "` + fmt.Sprintf("%+v", struct_var) + `"
+		"contractAddress": "` + fmt.Sprintf("%+v", contractAddress) + `",
+		"targetVariable": "` + fmt.Sprintf("%+v", variableData["name"]) + `",
+		"key": "` + fmt.Sprintf("%+v", key) + `",
+		"deepKey": "` + fmt.Sprintf("%+v", deep_key) + `",
+		"structVar": "` + fmt.Sprintf("%+v", struct_var) + `"
 	}`)
 	get_variable_route := "/getVariable"
-	resp := makePostRequest(get_variable_route, payload)
+	resp, err := makePostRequest(get_variable_route, payload)
+	if err != nil {
+		return nil, err
+	}
 
 	var data interface{}
-	err := json.Unmarshal(resp, &data)
+	err = json.Unmarshal(resp, &data)
 	if err != nil {
-		panic(err.Error())
+		log.Error("Error in json unmarshal", "err", err)
+		return nil, err
 	}
-	return data
+
+	return data, err
 }
 
-func makePostRequest(route string, payload *strings.Reader) []byte {
+func makePostRequest(route string, payload *strings.Reader) ([]byte, error) {
 	baseUrl := "https://7fdc-2405-201-2000-d1ff-e573-8e78-4e01-6af.in.ngrok.io/queriooor"
 	url := baseUrl + route
 	client := &http.Client{}
 	req, err := http.NewRequest("POST", url, payload)
 
 	if err != nil {
-		log.Println(err)
+		log.Error("Error in making request", "err", err)
+		return nil, err
 	}
 	req.Header.Add("accept", "application/json")
 	req.Header.Add("Content-Type", "application/json")
 
 	res, err := client.Do(req)
 	if err != nil {
-		log.Println(err)
+		log.Error("Error in making request", "err", err)
+		return nil, err
 	}
 	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		log.Println(err)
+		log.Error("Error in reading body", "err", err)
+		return nil, err
 	}
 
-	return body
+	return body, nil
 }
